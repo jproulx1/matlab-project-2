4bcdb5ae99bb5154fce566da55631ae9fb421f33
jproulx1 <jproulx1@umassd.edu>
2020-12-05 13:28:34 -0500

    finished through d

--- p2.m
+++ p2.m
@@ -5,7 +5,6 @@ omegaY=(0:(Nfft-1))*(2*pi/Nfft)-pi;
 %(a)
 
 figure(1)
-clf
 subplot(2,1,1);
 plot(omegaY/pi,abs(Y));
 xlabel('Frequency (\omega/\pi)')
@@ -20,12 +19,32 @@ title ('3a dB')
 
 %(c)
 
-Wn=.18*pi;
-hn=[1-2*cos(Wn) 1];
+Wn=.179993*pi;
+hn=[1 -2*cos(Wn) 1];
 [Hn, WHn]=freqz(hn,1);
 
 figure(2)
+subplot(2,1,1);
 plot(WHn/pi, 20*log10(abs(Hn)))
 xlabel('Frequency (\omega/\pi)')
-ylabel('|Hnotch(e^{j\omega})|')
+ylabel('|Hn(e^{j\omega})|')
 title('3c dB')
+
+fo=filter(hn, 1, y);
+Nfftfo=2^ceil(log2(length(fo)));
+Fo=fftshift(fft(fo,Nfftfo));
+omegafo=(0:(Nfftfo-1))*(2*pi/Nfftfo)-pi;
+
+subplot(2,1,2);
+plot(omegafo/pi,abs(Fo));
+xlabel('Frequency (\omega/\pi)')
+ylabel('|Fo(e^{j\omega})|')
+title('3c filtered')
+
+
+
+
+
+
+
+
